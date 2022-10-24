d4413f9ed89e7fd241651551c3c25c60f474d18d
Wellington Rodrigues <wellingtonrodrigues.rj@gmail.com>
2022-10-24 17:09:08 -0300

    middleware v1

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = new express();
+const cors = require("cors");
+const sfrouter = require('./rotas/sfrouter.js');
+
+app.use(cors());
+app.listen(555);
+
+app.use((req, res, next) => {
+    console.log('Time:', Date.now());
+    next();
+})
+
+app.use(sfrouter);
+
+app.use((req, res, next) => {
+    console.log('Time:', Date.now());
+})
---
+++ rotas/sfrouter.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const router = express.Router();
+
+// define o parse como application/json
+router.use(express.json());
+
+// define o parse como application/x-www-form-urlencoded
+router.use(express.urlencoded({ extended: false }));
+
+// a cada requisição gravar no mongodb
+router.post('/:pathname1/?:pathname2/?:pathname3/?:pathname4/?:pathname5', (req, res, next) => {
+    console.log('params:', req.params.pathname1);
+    console.log('params:', req.params.pathname2);
+    console.log('Request URL:', req.originalUrl);
+    console.log('method URL:', req.method);
+    console.log(req.body);
+    res.json(req.body);
+    next();
+});
+
+router.get('/:pathname', (req, res, next) => {
+    console.log('params:', req.params.pathname);
+    console.log('Request URL:', req.originalUrl);
+    console.log('method URL:', req.method);
+    console.log(req.body);
+    res.json(req.body);
+    next();
+});
+
+router.put('/:pathname', (req, res, next) => {
+    console.log('params:', req.params.pathname1);
+    console.log('Request URL:', req.originalUrl);
+    console.log('body:', req.body);
+    res.json(req.body);
+    next();
+});
+
+
+router.delete('/:pathname', (req, res, next) => {
+    console.log('params:', req.params.pathname);
+    console.log('Request URL:', req.originalUrl);
+    console.log('method URL:', req.method);
+    console.log(req.body);
+    res.json(req.body);
+    next();
+});
+
+module.exports = router;
