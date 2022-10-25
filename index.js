e26cf9aefe9a5617aa4d2ed2e3909c3f77b97b9f
Wellington Rodrigues <wellingtonrodrigues.rj@gmail.com>
2022-10-25 12:14:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = new express();
 const cors = require("cors");
 
+// https://acervolima.com/crie-um-aplicativo-simples-para-iniciantes-com-node-js-bootstrap-e-mongodb/
+
 const api = require('./rotas/api.js');
 
 app.use(cors());
