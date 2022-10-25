b747f02e5a11d25782e30435edf7582b609be0d5
Wellington Rodrigues <wellingtonrodrigues.rj@gmail.com>
2022-10-25 12:30:26 -0300

    Com porta dinamica

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
+require('dotenv').config();
 const express = require("express");
 const app = new express();
 const cors = require("cors");
+let porta = process.env.PORT || 8080;
 
 // https://acervolima.com/crie-um-aplicativo-simples-para-iniciantes-com-node-js-bootstrap-e-mongodb/
 
 const api = require('./rotas/api.js');
 
 app.use(cors());
-app.listen(555);
+app.listen(porta, ()=>{
+    console.log(`Serviço rodando na porta ${porta}`);
+});
 
 app.use((req, res, next) => {
-    console.log('Time:', Date.now());
+    console.log('Iniciando em :', new Date().toLocaleDateString());
     next();
 })
 
 app.use(api);
 
 app.use((req, res, next) => {
-    console.log('Time:', Date.now());
+    console.log('Finalizando em :', new Date().toLocaleDateString());
 })
